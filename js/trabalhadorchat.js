77f476df14eb30d8fd5e9ca057f425481a30ed2d
faelirado <159785876+faelirado@users.noreply.github.com>
2024-10-25 19:34:48 -0300

    Add files via upload

--- js/trabalhadorchat.js
+++ js/trabalhadorchat.js
@@ -6,10 +6,36 @@ if (voltar) {
   });
 }
 
+//mais notif que 99 fica 99+
 const qntNotifs = document.querySelectorAll("#qntNotifs")
 
 for (const notif of qntNotifs){
   if(notif.textContent > 99){
     notif.textContent = "+99"
   }
+  if(notif.textContent == 0){
+    notif.style.display = "none"
+  }
+}
+
+/*pesquisar*/
+function pesquisar() {
+  // Pega o valor da barra de pesquisa
+  let input = document.getElementById('barraProcurar').value.toLowerCase();
+  
+  // Seleciona todos os elementos com id "nomeProduto"
+  let mensagens = document.querySelectorAll('#mensagem');
+  
+  // Loop por todos os mensagens
+  mensagens.forEach(function(product) {
+      // Pega o texto do nome do produto
+      let nomeProduto = product.querySelector('#nomeCliente').textContent.toLowerCase();
+
+      // Verifica se o nome do produto contém o texto digitado
+      if (nomeProduto.includes(input)) {
+          product.style.display = 'block'; // Exibe o produto
+      } else {
+          product.style.display = 'none'; // Esconde o produto
+      }
+  });
 }
