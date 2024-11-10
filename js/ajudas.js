0c1707a0497a451cdf015670d5ef3bf624338612
faelirado <159785876+faelirado@users.noreply.github.com>
2024-11-10 12:11:18 -0300

    Add files via upload

--- js/ajudas.js
+++ js/ajudas.js
@@ -7,6 +7,36 @@ if (voltar) {
   });
 }
 
+/*pesquisar*/
+function pesquisar() {
+// Pega o valor da barra de pesquisa
+let input = document.getElementById('barraProcurar').value.toLowerCase();
+
+// Seleciona todos os elementos com id "nomeProduto"
+let servicos = document.querySelectorAll('#escolherajuda');
+
+// Loop por todos os servicos
+servicos.forEach(function(servico) {
+    // Pega o texto do nome do produto
+    let nomeServico = servico.querySelector('label').textContent.toLowerCase();
+
+    // Verifica se o nome do produto contém o texto digitado
+    if (nomeServico.includes(input)) {
+        servico.style.display = 'flex'; // Exibe o produto
+    } else {
+        servico.style.display = 'none'; // Esconde o produto
+    }
+});
+}
+
+const divchecks = document.querySelectorAll("#escolherajuda")
+
+divchecks.forEach(div => {
+    div.addEventListener("click",function(){
+        var check = this.querySelector("input")
+        check.checked = !check.checked
+    })
+})
 
 /*confere se há algum checkbox marcado*/
 document.getElementById("ajudas").addEventListener("submit", function(event) {
@@ -22,31 +52,39 @@ document.getElementById("ajudas").addEventListener("submit", function(event) {
         if (checkbox.checked) {
             isChecked = true;
             
-            var total = 0
+            var totalmin = 0
+            var totalmax = 100
             let valorMin = document.getElementById('valormin');
+            let valorMax = document.getElementById('valormax');
 
             checkboxes.forEach(function(checkbox) {
                 if (checkbox.checked) {
-                    total += parseFloat(checkbox.value);
+                    totalmin += parseFloat(checkbox.value);
+                    totalmax += parseFloat(checkbox.value);
                 }
             });
 
-            valorMin.textContent = total
+            valorMin.textContent = totalmin
+            valorMax.textContent = totalmax
             
             
             ajuda2.style.display = "block"
             ajuda1.style.transform = "translateX(-150dvw)"
             ajuda2.style.transform = "translateX(100%)"
-            setTimeout(function() {
-                ajuda1.style.display = "none"
-                ajuda2.style.transform = "translateX(-50%)"
-            }, 1400);
             
             setTimeout(function() {
-                valorOrcamento.style.display = 'block';
+                ajuda1.style.display = "none"
+                ajuda2.style.transform = "translateX(0)"
+            }, 1400);
+
+            setTimeout(function() {
+                valorOrcamento.style.display = "block"
+                document.getElementById("max").style.display = "block"
+                document.getElementById("min").style.display = "block"
             }, 1401);
 
             rangeOrcamento.min = valorMin.innerText
+            rangeOrcamento.max = valorMax.innerText
             valorOrcamento.textContent = `R$${rangeOrcamento.value}`;
 
             break;
--- js/contaconfig.js
+++ js/contaconfig.js
@@ -6,3 +6,31 @@ if (voltar) {
     location.href = "conta.html";
   });
 }
+const confirmarSaida = document.getElementById("confirmarSaida")
+
+document.getElementById("sairConta").addEventListener("click",()=>{
+  confirmarSaida.style.display = "block"
+})
+
+document.querySelector("#confirmarSaida #sim").addEventListener("click",()=>{
+  confirmarSaida.style.display = "none"
+  localStorage.setItem('image', '');
+  localStorage.setItem('valor-nome', '');
+  localStorage.setItem('valor-email', '');
+  localStorage.setItem('valor-senha', '');
+  localStorage.setItem('lembrar-login', '');
+  location.href = "index.html";
+})
+
+document.querySelector("#confirmarSaida #nao").addEventListener("click",()=>{
+  confirmarSaida.style.display = "none"
+})
+
+// Fecha o menu de confirmar ao clicar fora dele
+document.addEventListener('click', function(event) {
+  const isClickInsideConfirmarSaida = confirmarSaida.contains(event.target) || document.getElementById("sairConta").contains(event.target);
+  
+  if (!isClickInsideConfirmarSaida) {
+    confirmarSaida.style.display = "none"
+  }
+});
